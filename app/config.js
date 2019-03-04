02f6db420a9049d5021fd69a0187edec6232a01c
ericgroh <grohec.1995@gmail.com>
2019-03-04 14:34:22 -0500

    Update config.js

--- app/config.js
+++ app/config.js
@@ -3,4 +3,4 @@ dotenv.config();
 
 export const ENV = process.env.NODE_ENV || `development`;
 export const PORT = process.env.PORT || 4000;
-export const MONGO_URL  = process.env.MONGO_URL || `mongodb://localhost:27017/slam-mail`;
+export const MONGO_URL  = process.env.MONGO_URL || `mongodb://localhost:27017/something`;
